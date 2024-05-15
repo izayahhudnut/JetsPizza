fca962482f4c800fc9e372c07e4eef04648e175c
izayahhudnut <Izayahhudnut@gmail.com>
2024-05-15 14:49:23 -0500

    Added console.log to fix the 504 error in production

--- frontend/components/StartCard.tsx
+++ frontend/components/StartCard.tsx
@@ -17,10 +17,21 @@ export default function StartCard(parameters: any) {
             key={index}
             className="p-2 m-2 bg-custom-gray rounded-xl hover:cursor-pointer inline-flex"
             onClick={async () => {
-              const response = await submitUserMessage(
-                `The user has selected that they want to see products from this effect ${effect}`
-              );
-              setMessages((currentMessages: any[]) => [...currentMessages, response]);
+              try {
+                const response = await submitUserMessage(
+                  `The user has selected that they want to see products from this effect ${effect}`
+                );
+                console.log('Response:', response);
+                
+                // Check if response is iterable
+                if (Array.isArray(response)) {
+                  setMessages((currentMessages: any[]) => [...currentMessages, ...response]);
+                } else {
+                  setMessages((currentMessages: any[]) => [...currentMessages, response]);
+                }
+              } catch (error) {
+                console.error('Error submitting user message:', error);
+              }
             }}
           >
             <button className="text-sm text-gray-900">{effect}</button>
